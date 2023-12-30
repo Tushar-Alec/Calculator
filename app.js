290e6e99910e3f3f7951ab062133719dd63c5ad6
Tushar-Alec <petermoor822@gmail.com>
2023-12-30 18:30:44 +0530

    added update_display function.updated calculate function and made the calculator work.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let num1 = "";
+let num2 = "";
+let operator = "";
+
 const add = function (a, b) {
   return a + b;
 };
@@ -18,46 +22,81 @@ const divide = function (a, b) {
   }
 };
 
-let num1 = "1";
-let num2 = "1";
-let operator = "+";
-
 function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
       return add(num1, num2);
-      break;
     case "-":
       return subtract(num1, num2);
-      break;
     case "*":
       return multiply(num1, num2);
-      break;
     case "/":
       return divide(num1, num2);
-      break;
+    default:
+      return "Invalid operator";
   }
 }
 
-var displayValue = "";
-
-// function to display the typed value //
-
-function appendToDisplay(value) {
-  document.getElementById("display").value += value;
+// Function to update the display when a number button is clicked
+function appendToDisplay(num) {
+  if (operator === "") {
+    num1 += num;
+    updateDisplay(num1);
+  } else {
+    num2 += num;
+    updateDisplay(num2);
+  }
 }
 
-// func to clear display input //
+// Handle multiple decimal points
+function handleDecimal(value) {
+  if (!num1.includes(".") && !num2.includes(".")) {
+    if (operator === "") {
+      num1 += value;
+    } else {
+      num2 += value;
+    }
+    updateDisplay(value);
+  }
+}
 
+// Function to clear the display and reset variables
 function clearDisplay() {
   document.getElementById("display").value = "";
+  num1 = "";
+  num2 = "";
+  operator = "";
+  updateDisplay('');
 }
 
-// func to delete the last digit //
-
+// Function to delete the last digit from the display
 function DeleteLastDigit() {
   document.getElementById("display");
   display.value = display.value.slice(0, -1);
 }
 
-// function to do calculator operation //
+
+// Function to update the operator
+function setOperator(selectedOperator) {
+  if (num1 !== "") {
+    operator = selectedOperator;
+    
+  }
+}
+
+// Function to perform the calculation when the "=" key is pressed
+function calculate() {
+  if (num1 !== "" && num2 !== "" && operator !== "") {
+    let result = operate(
+      operator,
+      parseFloat(num1),
+      parseFloat(num2)
+    ).toString();
+    console.log("Result:", result);
+    document.getElementById("display").value = result;
+  }
+}
+
+function updateDisplay(value) {
+  document.getElementById("display").value = value;
+}
